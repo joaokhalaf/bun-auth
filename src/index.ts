deed1da7f4a8a59578bd005f0ac7cba590e3f880
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-09-30 23:51:18 -0300

    started api build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
+import openapi from "@elysiajs/openapi";
 import { Elysia } from "elysia";
+import { z } from 'zod';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.use(openapi())
+.get("/", () => "Hello Elysia")
+.get('/users/:id', ({params}) => {
+  const userId = params.id
+  return { id: userId, name: '..' }
+}, {
+  detail: {
+    summary: '..',
+    tags: ['', '']
+  },
+  params : z.object ({
+    id: z.string(),
+  }), 
+  response : {
+    200: z.object({
+      id: z.string(),
+      name: z.string()
+    })
+  }
+})
+.listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
