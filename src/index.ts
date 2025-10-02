0858582b8b7b4a198468a2226e75dda4ba3d786d
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-10-01 21:44:53 -0300

    fix(auth): cors config for local tests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import openapi from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 import { z } from 'zod';
+import { betterAuthPlugin, OpenAPI } from "./database/http/plugins/better-auth";
+import cors from "@elysiajs/cors";
 
 const app = new Elysia()
-.use(openapi())
+.use(cors({
+            origin: '*',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            credentials: true,
+            allowedHeaders: ['Content-Type', 'Authorization']
+        }))
+.use(openapi({
+        documentation: {
+            components: await OpenAPI.components,
+            paths: await OpenAPI.getPaths()
+        }
+    }))
+.use(betterAuthPlugin)
 .get("/", () => "Hello Elysia")
 .get('/users/:id', ({params}) => {
   const userId = params.id
